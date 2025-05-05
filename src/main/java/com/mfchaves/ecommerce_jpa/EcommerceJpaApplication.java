542bb968642783c6e6617347740d77170a3c8993
Mauricio Chaves <mauricio.chaves.jr@gmail.com>
2025-05-05 20:46:14 -0300

    Main: second commit

---
+++ src/main/java/com/mfchaves/ecommerce_jpa/EcommerceJpaApplication.java
@@ -0,0 +1,13 @@
+package com.mfchaves.ecommerce_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mfchaves/ecommerce_jpa/EcommerceJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfchaves.ecommerce_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
